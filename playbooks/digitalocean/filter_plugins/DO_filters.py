52fcb5ebcb06004f43e94a5b482f13587bd27fc6
John Morris <john@zultron.com>
2017-04-03 14:22:02 -0500

    Commit playbooks changes before destroying

--- playbooks/digitalocean/filter_plugins/DO_filters.py
+++ playbooks/digitalocean/filter_plugins/DO_filters.py
@@ -7,15 +7,15 @@ class FilterModule(object):
         items = [fmt.format(d) for d in data]
         return sep.join(items)
 
-    def hostname(self, fqdn):
-        return fqdn.split('.')[0]
+    # def hostname(self, fqdn):
+    #     return fqdn.split('.')[0]
 
-    def domainname(self, fqdn):
-        return '.'.join(fqdn.split('.')[1:])
+    # def domainname(self, fqdn):
+    #     return '.'.join(fqdn.split('.')[1:])
 
     def filters(self):
         return {
             'mapformat': self.mapformat,
-            'hostname': self.hostname,
-            'domainname': self.domainname,
+            # 'hostname': self.hostname,
+            # 'domainname': self.domainname,
         }
