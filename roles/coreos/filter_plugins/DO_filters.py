62816557ea37d493484b8df06327d38d122cbfed
John Morris <john@zultron.com>
2017-04-03 15:25:08 -0500

    Move cloud-config gen to coreos role; hostname tweaks

--- roles/coreos/filter_plugins/DO_filters.py
+++ roles/coreos/filter_plugins/DO_filters.py
@@ -7,15 +7,7 @@ class FilterModule(object):
         items = [fmt.format(d) for d in data]
         return sep.join(items)
 
-    # def hostname(self, fqdn):
-    #     return fqdn.split('.')[0]
-
-    # def domainname(self, fqdn):
-    #     return '.'.join(fqdn.split('.')[1:])
-
     def filters(self):
         return {
             'mapformat': self.mapformat,
-            # 'hostname': self.hostname,
-            # 'domainname': self.domainname,
         }
