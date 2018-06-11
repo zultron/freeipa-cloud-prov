5ad555060be0ec6caeb8c3986724c41315c70a3d
John Morris <john@zultron.com>
2018-06-11 16:31:23 -0500

    Commit lots of things from ages before
    I don't even remember what I was doing, but these are from when I gave
    up several months ago.

--- lib/filter_plugins/domain_ip_filters.py
+++ lib/filter_plugins/domain_ip_filters.py
@@ -8,25 +8,12 @@ class FilterModule(object):
         """
         return data.split('.')[0]
 
-    def reverse_ip(self, data, pad=False):
-        """Return the reverse of a set of dotted octets"""
-        octets = data.split('.')
-        if pad: octets = (octets + ['0'] * 4)[0:4]
-
-        return '.'.join(reversed(octets))
-
     def last_octet(self, data):
         """Return the last octet of a set of dotted octets"""
         return data.split('.')[-1]
 
-    def reverse_zone(self, data):
-        """Return the reverse zone of a set of dotted octets"""
-        return "{}.in-addr.arpa.".format(self.reverse_ip(data))
-
     def filters(self):
         return {
             'shortname': self.shortname,
-            'reverse_ip': self.reverse_ip,
-            'reverse_zone': self.reverse_zone,
             'last_octet': self.last_octet,
         }
